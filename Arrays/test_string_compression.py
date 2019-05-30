1e275fe72f705896c4289cbd1dce0c079e0f4d4e
Maria Pantsiou <maria.th.pantsiou@gmail.com>
2019-05-30 13:22:58 +0100

    String Compression

--- Arrays/test_string_compression.py
+++ Arrays/test_string_compression.py
@@ -9,6 +9,8 @@ takes more space.
 The function should also be case sensitive, so that a string 'AAAaaa' returns 'A3a3'.
 """
 
+
+# Doesn't work if the same letter is repeated more than one times across the string.
 from collections import defaultdict
 class Compression():
 
@@ -44,3 +46,51 @@ uncom_str = "AAAAAAVVVVVvvBBBBBBmmmmmmmmmm"
 compression = Compression(uncom_str)
 compression.compr_str()
 
+
+
+
+# """
+# Solution: The run length compression algorithm.
+# """
+
+# def compress(s):
+#     r = ""
+#     l = len(s)
+    
+#     if l==0:
+#         return ''
+#     if l==1:
+#         return s+'1'
+    
+#     cnt = 1
+#     i=1
+    
+#     while i<l:
+#         if s[i]==s[i-1]:
+#             cnt += 1
+#         else:
+#             r=r+s[i-1]+str(cnt)
+#             cnt=1
+#         i+=1
+#     r = r+s[i-1]+str(cnt)
+#     return r
+
+# compress('AAAAABBBBCCCC')
+
+
+# """
+# RUN THIS CELL TO TEST YOUR SOLUTION
+# """
+# from nose.tools import assert_equal
+
+# class TestCompress(object):
+
+#     def test(self, sol):
+#         assert_equal(sol(''), '')
+#         assert_equal(sol('AABBCC'), 'A2B2C2')
+#         assert_equal(sol('AAABCCDDDDD'), 'A3B1C2D5')
+#         print ('ALL TEST CASES PASSED')
+
+# # Run Tests
+# t = TestCompress()
+# t.test(compress)
